1ee61895a0f3910a223d0a227e8769565c0093ec
taherajna <Tmnagitb786!>
2020-06-14 15:03:24 +0530

    Added Junit for adding any amount of comma separated numbers

--- src/test/java/com/incubyte/tdd/StringCalculatorTest.java
+++ src/test/java/com/incubyte/tdd/StringCalculatorTest.java
@@ -32,7 +32,14 @@ public class StringCalculatorTest {
     @Test
     public void testAddForTwoNumbers() {
         Assert.assertEquals(3, stringCalculator.add("1,2"));
-        Assert.assertEquals(12+34, stringCalculator.add("12,34"));
+        Assert.assertEquals(12 + 34, stringCalculator.add("12,34"));
+    }
+
+
+    @Test
+    public void testAddFor_AnyAmountOfCommaSeparatedNumbers() {
+        Assert.assertEquals(1 + 2 + 3 + 4 + 5, stringCalculator.add("1,2,3,4,5"));
+        Assert.assertEquals(12 + 34 + 72 + 88, stringCalculator.add("12,34,72,88"));
     }
 
 }
