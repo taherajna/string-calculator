20ee1163e1d5d38c7125965ecfffecb66b361106
taherajna <Tmnagitb786!>
2020-06-14 14:46:01 +0530

    Added new class StringCalculator

---
+++ src/main/java/com/incubyte/tdd/StringCalculator.java
@@ -0,0 +1,4 @@
+package com.incubyte.tdd;
+
+public class StringCalculator {
+}
---
+++ src/test/java/com/incubyte/tdd/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package com.incubyte.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+    @Test
+    public void testClassExists() {
+        Assert.assertNotNull(new StringCalculator());
+    }
+}
