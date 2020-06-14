569b0fbc2a8ad589ac5a4d0623d3a35451c18608
taherajna <Tmnagitb786!>
2020-06-14 14:51:03 +0530

    Added test for add method in StringCalculator for empty string condition

--- src/main/java/com/incubyte/tdd/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/StringCalculator.java
@@ -1,4 +1,8 @@
 package com.incubyte.tdd;
 
 public class StringCalculator {
+
+    public int add(String s) {
+        return 0;
+    }
 }
--- src/test/java/com/incubyte/tdd/StringCalculatorTest.java
+++ src/test/java/com/incubyte/tdd/StringCalculatorTest.java
@@ -1,11 +1,25 @@
 package com.incubyte.tdd;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void testClassExists() {
-        Assert.assertNotNull(new StringCalculator());
+        Assert.assertNotNull(stringCalculator);
+    }
+
+    @Test
+    public void testAddForEmptyString() {
+        Assert.assertEquals(0, stringCalculator.add(""));
     }
 }
